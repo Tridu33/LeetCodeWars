c6a2879d25aa4f8343f828d9bc4a993133fe96a3
tridu33 <tridu33@qq.com>
2023-01-09 23:16:09 +0800

    scripts for github codespaces

---
+++ javaOJ/MvnCompile.bat
@@ -0,0 +1,3 @@
+"C:\Users\tridu33\AppData\Local\JetBrains\Toolbox\apps\IDEA-E\ch-0\222.4345.35\plugins\maven\lib\maven3\bin\mvn.cmd" clean compile
+@REM C:\Users\tridu33\AppData\Local\JetBrains\Toolbox\apps\IDEA-E\ch-0\222.4345.35\plugins\maven\lib\maven3\bin\mvn.cmd install
+@REM C:\Users\tridu33\AppData\Local\JetBrains\Toolbox\apps\IDEA-E\ch-0\222.4345.35\plugins\maven\lib\maven3\bin\mvn.cmd package
---
+++ javaOJ/RunMvn.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+"mvn" clean compile
+# mvn install
+# mvn package
+
+java "-cp" 'C:\tridu33\FPds\LeetCodeWars\javaOJ\target\classes' 'com.tridu33.mineOJ.Temp' 
